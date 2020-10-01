639588ad1951d160b510c7fb2a361b39cea7ce7f
Gabriel Gigante <gabriel_gigante@outlook.com>
2020-09-30 23:13:47 -0300

    Working with actions

--- src/components/Catalog/index.tsx
+++ src/components/Catalog/index.tsx
@@ -1,16 +1,23 @@
-import React, { useEffect, useState } from 'react';
-import { useSelector } from 'react-redux';
+import React, { useCallback, useEffect, useState } from 'react';
+import { useDispatch } from 'react-redux';
+
 import api from '../../services/api';
+import { addProductToCart } from '../../store/modules/cart/actions';
 import { IProduct } from '../../store/modules/cart/types';
 
 const Catalog: React.FC = () => {
+  const dispatch = useDispatch();
   const [catalog, setCatalog] = useState<IProduct[]>([]);
 
   useEffect(() => {
     api.get('products').then(response => {
       setCatalog(response.data);
     });
-  }, [])
+  }, []);
+
+  const handleAddProductToCart = useCallback((product) => {
+    dispatch(addProductToCart(product))
+  }, [dispatch]);
 
   return (
     <main>
@@ -21,7 +28,7 @@ const Catalog: React.FC = () => {
           <strong>{product.title}</strong> {" - "}
           <span>{product.price}</span> {" "}
 
-          <button type="button">Comprar</button>
+          <button onClick={() => handleAddProductToCart(product)} type="button">Comprar</button>
         </article>
       ))}
     </main>
